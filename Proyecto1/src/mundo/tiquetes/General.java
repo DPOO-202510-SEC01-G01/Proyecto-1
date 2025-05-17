059a761dc2aadbefa50f2b8356875ff518fc0271
sebastian-gfrd <sc.martineza12@uniandes.edu.co>
2025-05-17 09:50:48 -0500

    Ajuste sistema de niveles de tiquete

--- Proyecto1/src/mundo/tiquetes/General.java
+++ Proyecto1/src/mundo/tiquetes/General.java
@@ -1,17 +1,23 @@
 package tiquetes;
 
-import java.util.Date;
+import java.time.LocalDate;
 
 public class General extends Tiquete {
     private String tipo; // Básico, Familiar, Oro, Diamante
+    private int nivel;
 
-    public General(Date fechaCompra, int precio, String tipo) {
-        super(fechaCompra, precio);
+    public General(String fecha, int precio, String tipo, int nivel) {
+        super(fecha, precio);
         this.tipo = tipo;
+        this.nivel = nivel;
     }
 
     public String getTipo() {
         return tipo;
+    }
+    
+    public int getNivel() {
+    	return nivel;
     }
 
     @Override
