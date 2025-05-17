d52344fcc06aa9c9a8a60ea599db26006f2b519a
sebastian-gfrd <sc.martineza12@uniandes.edu.co>
2025-05-17 15:35:41 -0500

    Implementacion parcial Test ticket general

---
+++ Proyecto1/src/tests/tiquetes/test/GeneralTest.java
@@ -0,0 +1,37 @@
+package tiquetes.test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import tiquetes.General;
+
+public class GeneralTest {
+	General basico;
+	General familiar;
+	General oro;
+	General diamante;
+	@BeforeEach
+	void setUp() {
+		basico = new General(LocalDate.now(), 5, "basico", 0);
+		familiar = new General(LocalDate.now(), 10, "familiar", 0);
+		oro = new General(LocalDate.now(), 15, "oro", 0);
+		diamante = new General(LocalDate.now(), 20, "diamante", 0);
+	}
+	@Test
+	void getTipo() {
+		assertTrue(basico.getTipo().equals("basico"));
+		assertTrue(familiar.getTipo().equals("familiar"));
+		assertTrue(oro.getTipo().equals("oro"));
+		assertTrue(diamante.getTipo().equals("diamante"));
+	}
+	void getNivel() {
+		assertTrue(basico.getNivel()==0);
+		assertTrue(familiar.getNivel()==1);
+		assertTrue(oro.getNivel()==2);
+		assertTrue(diamante.getNivel()==3);
+	}
+}
